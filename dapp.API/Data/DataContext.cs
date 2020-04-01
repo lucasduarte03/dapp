97c0d2e5df5a74fa5892a0c908b850af659d12a2
Lucas <lucasduarte03@gmail.com>
2020-03-31 20:54:03 -0400

    Initial Commit

---
+++ dapp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using dapp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dapp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
